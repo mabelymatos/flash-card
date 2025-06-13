8317e86b6319740ead1ac6035c4d4192fce73800
mabelymatos <mabely.matos@escola.pr.gov.br>
2025-06-13 08:19:39 -0300

    codigo inicial

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,24 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'O Que Faz Esse Profissional?',
+    'O que um nail designer realmente faz?',
+    'Uma nail designer é uma artista que cria e aplica designs estéticos e artísticos nas unhas, usando diversas técnicas e materiais para deixá-las bonitas e personalizadas.'
 )
 
 criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Qual a Durabilidade?',
+    'Quanto tempo dura um alongamento de unhas feito por uma boa nail designer?',
+    'Geralmente, um alongamento bem feito pode durar de duas a três semanas antes de precisar de manutenção, dependendo do crescimento da sua unha e dos seus cuidados.
+'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Materiais Utilizados',
+    'Quais os principais materiais que uma nail designer usa?',
+    'Utilizamos uma variedade de produtos, como gel, fibra de vidro, esmaltes em gel, pós cromados, pedrarias e adesivos para criar os diferentes estilos.'
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Como Escolher o Melhor?',
+    'O que devo procurar ao escolher um nail designer? ',
+    'Procure por uma profissional com bom portfólio, que siga boas práticas de higiene, use materiais de qualidade e tenha boas recomendações de outros clientes.'
 )
